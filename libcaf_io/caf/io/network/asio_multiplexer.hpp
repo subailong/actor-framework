13632a4daa9876070e2383ead2b0e8c77f7af3e4
Joseph Noir <joseph.noir@gmail.com>
2015-04-14 17:58:50 +0200

    Add implementation for asio multiplexer scribes

--- libcaf_io/caf/io/network/asio_multiplexer.hpp
+++ libcaf_io/caf/io/network/asio_multiplexer.hpp
@@ -23,6 +23,8 @@
 
 #include "boost/asio.hpp"
 
+#include "caf/io/receive_policy.hpp"
+
 #include "caf/io/network/multiplexer.hpp"
 #include "caf/io/network/stream_manager.hpp"
 #include "caf/io/network/acceptor_manager.hpp"
@@ -32,6 +34,21 @@
 namespace caf {
 namespace io {
 namespace network {
+
+/**
+ * @brief Low-level backend for IO multiplexing.
+ */
+using io_backend = boost::asio::io_service;
+
+/**
+ * @brief Makes sure a {@link multiplexer} does not stop its event loop
+ *        before the application requests a shutdown.
+ *
+ * The supervisor informs the multiplexer in its constructor that it
+ * must not exit the event loop until the destructor of the supervisor
+ * has been called.
+ */
+using supervisor = boost::asio::io_service::work;
 
 /**
  * @brief Low-level socket type used as default.
@@ -53,8 +70,6 @@ using native_socket = typename default_socket::native_handle_type;
  */
 using native_socket_acceptor = typename default_socket_acceptor::native_handle_type;
 
-
-
 class asio_multiplexer : public multiplexer {
  public:
   friend class io::middleman;
@@ -62,21 +77,21 @@ class asio_multiplexer : public multiplexer {
 
   connection_handle new_tcp_scribe(const std::string&, uint16_t) override;
 
-  void assign_tcp_scribe(broker* ptr, connection_handle hdl) override;
+  void assign_tcp_scribe(broker*, connection_handle hdl) override;
 
-//  connection_handle add_tcp_scribe(broker*, default_socket_acceptor&& sock);
+  connection_handle add_tcp_scribe(broker*, default_socket&& sock);
 
   connection_handle add_tcp_scribe(broker*, native_socket fd) override;
 
-  connection_handle add_tcp_scribe(broker*, const std::string& h,
+  connection_handle add_tcp_scribe(broker*, const std::string& host,
                                    uint16_t port) override;
 
   std::pair<accept_handle, uint16_t>
   new_tcp_doorman(uint16_t p, const char* in, bool rflag) override;
 
-  void assign_tcp_doorman(broker* ptr, accept_handle hdl) override;
+  void assign_tcp_doorman(broker*, accept_handle hdl) override;
 
-//  accept_handle add_tcp_doorman(broker*, default_socket_acceptor&& sock);
+  accept_handle add_tcp_doorman(broker*, default_socket_acceptor&& sock);
 
   accept_handle add_tcp_doorman(broker*, native_socket fd) override;
 
@@ -95,9 +110,335 @@ class asio_multiplexer : public multiplexer {
 
  private:
 
+  inline boost::asio::io_service& backend() {
+    return m_backend;
+  }
+
+  io_backend m_backend;
+  std::mutex m_mtx;
+  std::map<int64_t,default_socket> m_unassigned_sockets;
 };
 
 asio_multiplexer& get_multiplexer_singleton();
+
+template <class T>
+connection_handle conn_hdl_from_socket(T& sock) {
+  return connection_handle::from_int(
+    int64_from_native_socket(sock.native_handle()));
+}
+
+template <class T>
+accept_handle accept_hdl_from_socket(T& sock) {
+  return accept_handle::from_int(
+        int64_from_native_socket(sock.native_handle()));
+}
+
+// not sure about this
+
+/**
+ * @relates manager
+ */
+using manager_ptr = intrusive_ptr<manager>;
+
+/**
+ * @brief A stream capable of both reading and writing. The stream's input
+ *        data is forwarded to its {@link stream_manager manager}.
+ */
+template<class Socket>
+class stream {
+
+ public:
+
+    /**
+     * @brief A smart pointer to a stream manager.
+     */
+    using manager_ptr = intrusive_ptr<stream_manager>;
+
+    /**
+     * @brief A buffer class providing a compatible
+     *        interface to @p std::vector.
+     */
+    using buffer_type = std::vector<char>;
+
+    stream(io_backend& backend) : m_writing(false), m_fd(backend) {
+        configure_read(receive_policy::at_most(1024));
+    }
+
+    /**
+     * @brief Returns the @p multiplexer this stream belongs to.
+     */
+    inline io_backend& backend() {
+        return m_fd.get_io_service();
+    }
+
+    /**
+     * @brief Returns the IO socket.
+     */
+    inline Socket& socket_handle() {
+        return m_fd;
+    }
+
+    /**
+     * @brief Initializes this stream, setting the socket handle to @p fd.
+     */
+    void init(Socket fd) {
+        m_fd = std::move(fd);
+    }
+
+    /**
+     * @brief Starts reading data from the socket, forwarding incoming
+     *        data to @p mgr.
+     */
+    void start(const manager_ptr& mgr) {
+        // BOOST_ACTOR_REQUIRE(mgr != nullptr);
+        read_loop(mgr);
+    }
+
+    /**
+     * @brief Configures how much data will be provided
+     *        for the next @p consume callback.
+     * @warning Must not be called outside the IO multiplexers event loop
+     *          once the stream has been started.
+     */
+    void configure_read(receive_policy::config config) {
+        m_rd_flag = config.first;
+        m_rd_size = config.second;
+    }
+
+    /**
+     * @brief Copies data to the write buffer.
+     * @note Not thread safe.
+     */
+    void write(const void* buf, size_t num_bytes) {
+        auto first = reinterpret_cast<const char*>(buf);
+        auto last  = first + num_bytes;
+        m_wr_offline_buf.insert(first, last);
+    }
+
+    /**
+     * @brief Returns the write buffer of this stream.
+     * @warning Must not be modified outside the IO multiplexers event loop
+     *          once the stream has been started.
+     */
+    buffer_type& wr_buf() {
+        return m_wr_offline_buf;
+    }
+
+    buffer_type& rd_buf() {
+        return m_rd_buf;
+    }
+
+    /**
+     * @brief Sends the content of the write buffer, calling
+     *        the @p io_failure member function of @p mgr in
+     *        case of an error.
+     * @warning Must not be called outside the IO multiplexers event loop
+     *          once the stream has been started.
+     */
+    void flush(const manager_ptr& mgr) {
+        // BOOST_ACTOR_REQUIRE(mgr != nullptr);
+        if (!m_wr_offline_buf.empty() && !m_writing) {
+            m_writing = true;
+            write_loop(mgr);
+        }
+    }
+
+    /**
+     * @brief Closes the network connection, thus stopping this stream.
+     */
+    void stop() {
+        CAF_LOGMF(CAF_TRACE, "");
+        m_fd.close();
+    }
+
+    void stop_reading() {
+        CAF_LOGMF(CAF_TRACE, "");
+        boost::system::error_code ec; // ignored
+        m_fd.shutdown(boost::asio::ip::tcp::socket::shutdown_receive, ec);
+    }
+
+ private:
+
+    void write_loop(const manager_ptr& mgr) {
+        if (m_wr_offline_buf.empty()) {
+            m_writing = false;
+            return;
+        }
+        m_wr_buf.clear();
+        m_wr_buf.swap(m_wr_offline_buf);
+        boost::asio::async_write(m_fd, boost::asio::buffer(m_wr_buf),
+                          [=](const boost::system::error_code& ec, size_t nb) {
+            CAF_LOGC(CAF_TRACE, "caf::io::network::stream",
+                                "write_loop$lambda",
+                                BOOST_ACTOR_ARG(this));
+            static_cast<void>(nb); // silence compiler warning
+            if (!ec) {
+                CAF_LOGC(CAF_DEBUG, "caf::io::network::stream",
+                                    "write_loop$lambda",
+                                    nb << " bytes sent");
+                write_loop(mgr);
+            }
+            else {
+                CAF_LOGC(CAF_DEBUG, "caf::io::network::stream",
+                                    "write_loop$lambda",
+                                    "error during send: " << ec.message());
+                mgr->io_failure(operation::read);
+                m_writing = false;
+            }
+        });
+    }
+
+    void read_loop(const manager_ptr& mgr) {
+        auto cb = [=](const boost::system::error_code& ec, size_t read_bytes) {
+            CAF_LOGC(CAF_TRACE, "caf::io::network::stream",
+                                "read_loop$cb",
+                                CAF_ARG(this));
+            if (!ec) {
+                mgr->consume(m_rd_buf.data(), read_bytes);
+                read_loop(mgr);
+            }
+            else mgr->io_failure(operation::read);
+        };
+        switch (m_rd_flag) {
+            case receive_policy_flag::exactly:
+                if (m_rd_buf.size() < m_rd_size) m_rd_buf.resize(m_rd_size);
+                boost::asio::async_read(m_fd, boost::asio::buffer(m_rd_buf, m_rd_size), cb);
+                break;
+            case receive_policy_flag::at_most:
+                if (m_rd_buf.size() < m_rd_size) m_rd_buf.resize(m_rd_size);
+                m_fd.async_read_some(boost::asio::buffer(m_rd_buf, m_rd_size), cb);
+                break;
+            case receive_policy_flag::at_least: {
+                // read up to 10% more, but at least allow 100 bytes more
+                auto min_size =   m_rd_size
+                                + std::max<size_t>(100, m_rd_size / 10);
+                if (m_rd_buf.size() < min_size) m_rd_buf.resize(min_size);
+                collect_data(mgr, 0);
+                break;
+            }
+        }
+    }
+
+    void collect_data(const manager_ptr& mgr, size_t collected_bytes) {
+        m_fd.async_read_some(boost::asio::buffer(m_rd_buf.data() + collected_bytes,
+                                          m_rd_buf.size() - collected_bytes),
+                            [=](const boost::system::error_code& ec, size_t nb) {
+            CAF_LOGC(CAF_TRACE, "caf::io::network::stream",
+                                "collect_data$lambda",
+                                CAF_ARG(this));
+            if (!ec) {
+                auto sum = collected_bytes + nb;
+                if (sum >= m_rd_size) {
+                    mgr->consume(m_rd_buf.data(), sum);
+                    read_loop(mgr);
+                }
+                else collect_data(mgr, sum);
+            }
+            else mgr->io_failure(operation::write);
+        });
+    }
+
+    bool                m_writing;
+    Socket              m_fd;
+    receive_policy_flag m_rd_flag;
+    size_t              m_rd_size;
+    buffer_type         m_rd_buf;
+    buffer_type         m_wr_buf;
+    buffer_type         m_wr_offline_buf;
+
+};
+
+/**
+ * @brief An acceptor is responsible for accepting incoming connections.
+ */
+template<class SocketAcceptor>
+class acceptor {
+
+    using protocol_type = typename SocketAcceptor::protocol_type;
+    using socket_type   = boost::asio::basic_stream_socket<protocol_type>;
+
+ public:
+
+    /**
+     * @brief A manager providing the @p accept member function.
+     */
+    using manager_type = acceptor_manager;
+
+    /**
+     * @brief A smart pointer to an acceptor manager.
+     */
+    using manager_ptr = intrusive_ptr<manager_type>;
+
+    acceptor(io_backend& backend)
+    : m_backend(backend), m_accept_fd(backend), m_fd(backend) { }
+
+    /**
+     * @brief Returns the @p multiplexer this acceptor belongs to.
+     */
+    inline io_backend& backend() {
+        return m_backend;
+    }
+
+    /**
+     * @brief Returns the IO socket.
+     */
+    inline SocketAcceptor& socket_handle() {
+        return m_accept_fd;
+    }
+
+    /**
+     * @brief Returns the accepted socket. This member function should
+     *        be called only from the @p new_connection callback.
+     */
+    inline socket_type& accepted_socket() {
+        return m_fd;
+    }
+
+    /**
+     * @brief Initializes this acceptor, setting the socket handle to @p fd.
+     */
+    void init(SocketAcceptor fd) {
+        m_accept_fd = std::move(fd);
+    }
+
+    /**
+     * @brief Starts this acceptor, forwarding all incoming connections to
+     *        @p manager. The intrusive pointer will be released after the
+     *        acceptor has been closed or an IO error occured.
+     */
+    void start(const manager_ptr& mgr) {
+        // BOOST_ACTOR_REQUIRE(mgr != nullptr);
+        accept_loop(mgr);
+    }
+
+    /**
+     * @brief Closes the network connection, thus stopping this acceptor.
+     */
+    void stop() {
+        m_accept_fd.close();
+    }
+
+ private:
+
+    void accept_loop(const manager_ptr& mgr) {
+        m_accept_fd.async_accept(m_fd, [=](const boost::system::error_code& ec) {
+            CAF_LOGMF(CAF_TRACE, "");
+            if (!ec) {
+                mgr->new_connection(); // probably moves m_fd
+                // reset m_fd for next accept operation
+                m_fd = socket_type{m_accept_fd.get_io_service()};
+                accept_loop(mgr);
+            }
+            else mgr->io_failure(operation::read);
+        });
+    }
+
+    io_backend&   m_backend;
+    SocketAcceptor m_accept_fd;
+    socket_type    m_fd;
+
+};
+
 
 
 } // namesapce network
--- libcaf_io/src/asio_multiplexer.cpp
+++ libcaf_io/src/asio_multiplexer.cpp
@@ -18,72 +18,160 @@
  * http://www.boost.org/LICENSE_1_0.txt.                                      *
  ******************************************************************************/
 
+#include "caf/exception.hpp"
+
+#include "caf/io/broker.hpp"
+
 #include "caf/io/network/asio_multiplexer.hpp"
 
 namespace caf {
 namespace io {
 namespace network {
 
-  connection_handle asio_multiplexer::new_tcp_scribe(const std::string&,
-                                                     uint16_t) {
 
+default_socket new_tcp_connection(io_backend& backend,
+                                  const std::string& host, uint16_t port) {
+  default_socket fd{backend};
+  using boost::asio::ip::tcp;
+  try {
+    tcp::resolver r(fd.get_io_service());
+    tcp::resolver::query q(host, std::to_string(port));
+    auto i = r.resolve(q);
+    boost::asio::connect(fd, i);
   }
-
-  void asio_multiplexer::assign_tcp_scribe(broker* ptr, connection_handle hdl) {
-
+  catch (boost::system::system_error& se) {
+    throw network_error(se.code().message());
   }
+  return fd;
+}
 
-//  connection_handle add_tcp_scribe(broker*, default_socket_acceptor&& sock);
+connection_handle asio_multiplexer::new_tcp_scribe(const std::string& host,
+                                                   uint16_t port) {
+  default_socket fd{new_tcp_connection(backend(), host, port)};
+  auto id = int64_from_native_socket(fd.native_handle());
+  std::lock_guard<std::mutex> lock(m_mtx);
+  m_unassigned_sockets.insert(std::make_pair(id, std::move(fd)));
+  return connection_handle::from_int(id);
+}
 
-  connection_handle asio_multiplexer::add_tcp_scribe(broker*,
-                                                     native_socket fd) {
-
+void asio_multiplexer::assign_tcp_scribe(broker* self, connection_handle hdl) {
+  std::lock_guard<std::mutex> lock(m_mtx);
+  auto itr = m_unassigned_sockets.find(hdl.id());
+  if (itr == m_unassigned_sockets.end()) {
+    add_tcp_scribe(self, std::move(itr->second));
+    m_unassigned_sockets.erase(itr);
   }
+}
 
-  connection_handle asio_multiplexer::add_tcp_scribe(broker*, const std::string& h,
-                                                     uint16_t port) {
+connection_handle asio_multiplexer::add_tcp_scribe(broker* self,
+                                                   default_socket&& sock) {
+  CAF_LOG_TRACE("");
+  class impl : public broker::scribe {
+   public:
+    impl(broker* ptr, default_socket&& s)
+      : scribe(ptr, network::conn_hdl_from_socket(s)),
+        m_launched(false),
+        m_stream(s.get_io_service()) {
+      m_stream.init(std::move(s));
+    }
+    void configure_read(receive_policy::config config) override {
+      CAF_LOG_TRACE("");
+      m_stream.configure_read(config);
+      if (!m_launched) launch();
+    }
+    broker::buffer_type& wr_buf() override {
+      return m_stream.wr_buf();
+    }
+    broker::buffer_type& rd_buf() override {
+      return m_stream.rd_buf();
+    }
+    void stop_reading() override {
+      CAF_LOG_TRACE("");
+      m_stream.stop_reading();
+      disconnect(false);
+    }
+    void flush() override {
+      CAF_LOG_TRACE("");
+      m_stream.flush(this);
+    }
+    void launch() {
+      CAF_LOG_TRACE("");
+      CAF_ASSERT(!m_launched);
+      m_launched = true;
+      m_stream.start(this);
+    }
+   private:
+    bool m_launched;
+    stream<default_socket> m_stream;
+  };
+  broker::scribe_pointer ptr = make_counted<impl>(self, std::move(sock));
+  self->add_scribe(ptr);
+  return ptr->hdl();
+}
 
+connection_handle asio_multiplexer::add_tcp_scribe(broker* self,
+                                                   native_socket fd) {
+  CAF_LOG_TRACE(CAF_ARG(self) << ", " << CAF_ARG(fd));
+  boost::system::error_code error;
+  boost::asio::ip::tcp::socket sock(backend());
+  auto ec = sock.assign(boost::asio::ip::tcp::v6(), fd, error);
+  if (ec || error) {
+    ec = sock.assign(boost::asio::ip::tcp::v4(), fd, error);
   }
-
-  std::pair<accept_handle, uint16_t>
-  asio_multiplexer::new_tcp_doorman(uint16_t p, const char* in, bool rflag) {
-
+  if (ec || error) {
+    throw network_error(ec.message());
   }
+  return add_tcp_scribe(self, std::move(sock));
+}
 
-  void asio_multiplexer::assign_tcp_doorman(broker* ptr, accept_handle hdl) {
+connection_handle asio_multiplexer::add_tcp_scribe(broker* self,
+                                                   const std::string& host,
+                                                   uint16_t port) {
+  CAF_LOG_TRACE(CAF_ARG(self) << ", " << CAF_ARG(host)
+                << ", " << CAF_ARG(port));
+  return add_tcp_scribe(self, new_tcp_connection(backend(), host, port));
+}
 
-  }
+std::pair<accept_handle, uint16_t>
+asio_multiplexer::new_tcp_doorman(uint16_t p, const char* in, bool rflag) {
+
+}
+
+void asio_multiplexer::assign_tcp_doorman(broker* ptr, accept_handle hdl) {
+
+}
 
 //  accept_handle add_tcp_doorman(broker*, default_socket_acceptor&& sock);
 
-  accept_handle asio_multiplexer::add_tcp_doorman(broker*, native_socket fd) {
+accept_handle asio_multiplexer::add_tcp_doorman(broker*, native_socket fd) {
 
-  }
+}
 
-  std::pair<accept_handle, uint16_t>
-  asio_multiplexer::add_tcp_doorman(broker*, uint16_t p, const char* in, bool rflag) {
+std::pair<accept_handle, uint16_t>
+asio_multiplexer::add_tcp_doorman(broker*, uint16_t p,
+                                  const char* in, bool rflag) {
 
-  }
+}
 
-  void asio_multiplexer::dispatch_runnable(runnable_ptr ptr) {
+void asio_multiplexer::dispatch_runnable(runnable_ptr ptr) {
 
-  }
+}
 
-  asio_multiplexer::asio_multiplexer() {
+asio_multiplexer::asio_multiplexer() {
 
-  }
+}
 
-  asio_multiplexer::~asio_multiplexer() {
+asio_multiplexer::~asio_multiplexer() {
 
-  }
+}
 
-  multiplexer::supervisor_ptr asio_multiplexer::make_supervisor() {
+multiplexer::supervisor_ptr asio_multiplexer::make_supervisor() {
 
-  }
+}
 
-  void asio_multiplexer::run() {
+void asio_multiplexer::run() {
 
-  }
+}
 
 } // namesapce network
 } // namespace io
